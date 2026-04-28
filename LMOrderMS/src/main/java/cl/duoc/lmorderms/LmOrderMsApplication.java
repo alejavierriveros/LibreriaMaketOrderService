0dfce20e030cdddca7b095ca313403e543f676d6
LenavarrDuoc <leo.navarro@duocuc.cl>
2026-04-28 00:52:43 -0400

    -Primer commit de MicroServicio Orden. -Solo Estructura básica de Spring Boot.

---
+++ LMOrderMS/src/main/java/cl/duoc/lmorderms/LmOrderMsApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.lmorderms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmOrderMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmOrderMsApplication.class, args);
+    }
+
+}
---
+++ LMOrderMS/src/test/java/cl/duoc/lmorderms/LmOrderMsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.lmorderms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmOrderMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
